5350b4fff1fbfacc8f5a240fda2394336a6245cb
dinith114 <dinith.gsw@gmail.com>
2026-03-23 09:55:01 +0530

    Add initial setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-white text-black">
+      <h1 className="text-4xl font-bold text-center py-10">
+        Hotel Website Frontend Setup Working
+      </h1>
+    </div>
+  )
+}
+
+export default App
